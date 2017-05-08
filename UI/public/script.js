41dec1f99e6feaf199d97fffaf273adf1e40fdcf
ambrozhevich <tambrojevich@gmail.com>
2017-05-08 14:04:58 +0300

    9 task

--- UI/public/script.js
+++ UI/public/script.js
@@ -1,15 +1,13 @@
-'uses strict'
+'uses strict';
 
-var username = null;
-
-
-var articlesService = (function () {
+let username = null;
 
 
+const articlesService = (function () {
     function getArticles(skip, top, filterConfig) {
         skip = skip || 0;
         top = top || 10;
-        var sortedArticles = serverWorker.getArticles().slice();
+        let sortedArticles = serverWorker.getArticles().slice();
 
         if (filterConfig) {
             if (filterConfig.author) {
@@ -22,25 +20,25 @@ var articlesService = (function () {
         return sortedArticles.splice(skip, top);
     }
 
-    var validator = {
-        id: function (id) {
-            return Boolean(id) && typeof(id) === 'string';
+    const validator = {
+        id(id) {
+            return Boolean(id) && typeof (id) === 'string';
         },
-        title: function (title) {
-            return Boolean(title) && typeof(title) === 'string' && title.length < 100;
+        title(title) {
+            return Boolean(title) && typeof (title) === 'string' && title.length < 100;
         },
-        summary: function (summary) {
-            return Boolean(summary) && typeof(summary) === 'string' && summary.length < 200;
+        summary(summary) {
+            return Boolean(summary) && typeof (summary) === 'string' && summary.length < 200;
         },
-        createdAt: function (createdAt) {
+        createdAt(createdAt) {
             return Boolean(createdAt) && createdAt instanceof Date;
         },
-        author: function (author) {
-            return Boolean(author) && typeof(author) === 'string';
+        author(author) {
+            return Boolean(author) && typeof (author) === 'string';
         },
-        content: function (content) {
-            return Boolean(content) && typeof(content) === 'string';
-        }
+        content(content) {
+            return Boolean(content) && typeof (content) === 'string';
+        },
     };
 
     function validateArticle(article) {
@@ -56,14 +54,13 @@ var articlesService = (function () {
         if (validateArticle(article)) {
             articles.push(article);
             return true;
-        } else
-            return false;
+        } return false;
     }
 
     function editArticle(id, article) {
-        var amendArticle;
+        let amendArticle;
         amendArticle = serverWorker.getArticle(id);
-        var index = articles.indexOf(amendArticle);
+        const index = articles.indexOf(amendArticle);
         if (article.title != null) {
             amendArticle.title = article.title;
         }
@@ -73,82 +70,56 @@ var articlesService = (function () {
         if (article.content != null) {
             amendArticle.content = article.content;
         }
-        if (!validateArticle(amendArticle))
-            return false;
-        Object.assign(article, amendArticle)
+        if (!validateArticle(amendArticle)) { return false; }
+        Object.assign(article, amendArticle);
         articles[index] = article;
         functionsService.change(articles[index].id, articles[index]);
         return true;
     }
 
     return {
-        getArticles: getArticles,
+        getArticles,
         add: addArticle,
         edit: editArticle,
-        size: function () {
+        size() {
             return serverWorker.getArticles().length;
-        }
+        },
     };
 }());
 
 var functionsService = (function () {
-    var id = 1;
+    const id = 1;
 
-    var range = {
+    const range = {
         skip: 0,
-        top: 3
+        top: 3,
     };
 
     function addNew(id) {
-        var article = serverWorker.getArticle(id);
-        if (!article)
-            return false;
-        var elem = document.createElement('div');
-        elem.className = "new";
+        const article = serverWorker.getArticle(id);
+        if (!article) { return false; }
+        const elem = document.createElement('div');
+        elem.className = 'new';
         elem.id = id;
         elem.innerHTML =
-            (Boolean(article.image) ? '<img style="width: 100%; height: 200px; object-fit: cover;" src="' + article.image + '">' : '') +
-            '<h3>' + article.title + ' </h3>' +
-            '<p class="description" > ' + article.summary + '</p>' +
-            '<p class="author"> ' + 'Author: ' + article.author + ' </p>' +
-            '<p class="link" href="" onclick="ui.openReadMoreForm(' + id + ');">Read more...</p>' +
+            `${article.image ? `<img style="width: 100%; height: 200px; object-fit: cover;" src="${article.image}">` : ''
+            }<h3>${article.title} </h3>` +
+            `<p class="description" > ${article.summary}</p>` +
+            '<p class="author"> ' + `Author: ${article.author} </p>` +
+            `<p class="link" href="" onclick="ui.openReadMoreForm(${id});">Read more...</p>` +
             ' <div class="icons">' +
-            '<button style="display:' + (username ? 'inline' : 'none') + ';" id="' + elem.id + '" class="icon1" onclick="ui.openEditForm(this.id);"> <img src="change1.png" width="15" height="18"></button>' +
-            '<button style="display:' + (username ? 'inline' : 'none') + ';" id="' + elem.id + '" class="icon2" onclick="functionsService.delete(this.id);"> <img src="delete1.png" width="15" height="18"></button>' +
+            `<button style="display:${username ? 'inline' : 'none'};" id="${elem.id}" class="icon1" onclick="ui.openEditForm(this.id);"> <img src="change1.png" width="15" height="18"></button>` +
+            `<button style="display:${username ? 'inline' : 'none'};" id="${elem.id}" class="icon2" onclick="functionsService.delete(this.id);"> <img src="delete1.png" width="15" height="18"></button>` +
             '</div>';
         id++;
         elem.style.width = '235px';
         elem.style.height = 'auto';
         return elem;
     }
-
-    function deleteNew(id) {
-        serverWorker.removeArticle(id);
-        show();
-    }
-
-    var currentFilter = {};
-    var len = articlesService.size();
-
-    function filter() {
-        var date = document.querySelector('input.menu').value;
-        if (!date) date = undefined;
-        else date = new Date(date);
-        var author = document.querySelector('#by-author').value;
-        if (!author) author = undefined;
-        if (author === 'Select author') author = undefined;
-
-        currentFilter = currentFilter || {};
-        currentFilter.dateFrom = date;
-        currentFilter.author = author;
-
-        range.skip = 0;
-        len = articlesService.getArticles(0, articlesService.size(), currentFilter).length;
-        show();
-    }
-
+    let currentFilter = {};
+    let len = articlesService.size();
     function show(what) {
-        var parentElem = document.getElementsByClassName("news")[0];
+        const parentElem = document.getElementsByClassName('news')[0];
         parentElem.innerHTML = '';
 
         if (what === 'next') {
@@ -157,7 +128,7 @@ var functionsService = (function () {
         if (what === 'prev') {
             range.skip = Math.max(0, range.skip - 3);
         }
-        var elem = articlesService.getArticles(range.skip, range.top, currentFilter);
+        const elem = articlesService.getArticles(range.skip, range.top, currentFilter);
         if (len <= range.skip + range.top) {
             document.querySelector('#show-more').style.display = 'none';
         } else document.querySelector('#show-more').style.display = '';
@@ -170,42 +141,62 @@ var functionsService = (function () {
             parentElem.appendChild(addNew(elem[field].id));
         }
     }
+    function deleteNew(id) {
+        serverWorker.removeArticle(id);
+        show();
+    }
+    function filter() {
+        let date = document.querySelector('input.menu').value;
+        if (!date) date = undefined;
+        else date = new Date(date);
+        let author = document.querySelector('#by-author').value;
+        if (!author) author = undefined;
+        if (author === 'Select author') author = undefined;
+
+        currentFilter = currentFilter || {};
+        currentFilter.dateFrom = date;
+        currentFilter.author = author;
+
+        range.skip = 0;
+        len = articlesService.getArticles(0, articlesService.size(), currentFilter).length;
+        show();
+    }
+
 
     function sign(password) {
         if (username && users[username] === password) {
-            var button1 = document.querySelector('#user .pink.btn');
-            var button2 = document.querySelector('#user .exit');
+            const button1 = document.querySelector('#user .pink.btn');
+            const button2 = document.querySelector('#user .exit');
             button1.style.display = 'none';
             button2.style.display = 'inline';
 
-            var userTemp = document.getElementById('USERNAME');
-            userTemp.innerHTML = '<span>' + username + '</span>';
+            const userTemp = document.getElementById('USERNAME');
+            userTemp.innerHTML = `<span>${username}</span>`;
             userTemp.style.display = 'block';
 
-            var buttons = document.getElementsByClassName("icons");
-            for (var i = 0; i < buttons.length; i++) {
-                buttons.item(i).style.display = "inline";
+            const buttons = document.getElementsByClassName('icons');
+            for (let i = 0; i < buttons.length; i++) {
+                buttons.item(i).style.display = 'inline';
             }
 
-            var add = document.getElementsByClassName("add");
-            add[0].style.display = "inline";
+            const add = document.getElementsByClassName('add');
+            add[0].style.display = 'inline';
             return true;
         } else if (!username) {
-            var button1 = document.querySelector('#user .pink.btn');
-            var button2 = document.querySelector('#user .exit');
+            const button1 = document.querySelector('#user .pink.btn');
+            const button2 = document.querySelector('#user .exit');
             button1.style.display = 'inline';
             button2.style.display = 'none';
 
-            document.getElementById("USERNAME").style.display = 'none';
+            document.getElementById('USERNAME').style.display = 'none';
 
-            var buttons = document.getElementsByClassName("icons");
-            for (var i = 0; i < buttons.length; i++) {
-                buttons.item(i).style.display = "none";
+            const buttons = document.getElementsByClassName('icons');
+            buttons.item(i).style.display = 'none';
+            for (const i = 0; i < buttons.length; i++) {
             }
 
-            var add = document.getElementsByClassName("add");
-            add[0].style.display = "none";
-
+            const add = document.getElementsByClassName('add');
+            add[0].style.display = 'none';
         } else {
             alert('incorrect');
         }
@@ -213,20 +204,20 @@ var functionsService = (function () {
     }
 
     function change(id, article) {
-        var result = document.getElementById(article.id);
+        const result = document.getElementById(article.id);
         if (result != null) {
-            var temp = addNew(article);
+            const temp = addNew(article);
             result.class = temp.class;
             result.innerHTML = temp.innerHTML;
         }
     }
 
-    var AUTHORS = ['TUT.BY', 'Дмитрий Корсак', 'SPORT.TUT.BY', 'Никита Мелкозеров', 'Виталий Олехнович', 'Onliner.by', '42.TUT.BY', 'tambrozhevich'];
+    const AUTHORS = ['TUT.BY', 'Дмитрий Корсак', 'SPORT.TUT.BY', 'Никита Мелкозеров', 'Виталий Олехнович', 'Onliner.by', '42.TUT.BY', 'tambrozhevich'];
 
     function formSelection() {
-        for (var i = 0; i < AUTHORS.length; i++) {
-            var select = document.getElementById('by-author');
-            var option = document.createElement('option');
+        for (let i = 0; i < AUTHORS.length; i++) {
+            const select = document.getElementById('by-author');
+            const option = document.createElement('option');
             option.innerHTML = AUTHORS[i];
             select.appendChild(option);
         }
@@ -236,43 +227,42 @@ var functionsService = (function () {
 
     function addSpecificNew(article) {
         article.createdAt = new Date();
-        var result = document.getElementsByClassName('news');
+        const result = document.getElementsByClassName('news');
         result[0].insertBefore(addNew(article), result[0].firstChild);
     }
 
     return {
-        show: show,
-        sign: sign,
+        show,
+        sign,
         add: addNew,
         addNewElem: addSpecificNew,
         select: formSelection,
         delete: deleteNew,
-        change: change,
-        filter: filter
+        change,
+        filter,
     };
-
 }());
 
-var ui = (function () {
+const ui = (function () {
     function openSignInForm() {
-        var copy = document.querySelector('#sign-in-form').content.cloneNode(true);
+        const copy = document.querySelector('#sign-in-form').content.cloneNode(true);
         document.querySelector('#overlay').style.display = 'flex';
         document.querySelector('#overlay').appendChild(copy);
         document.querySelector('#overlay').firstElementChild.animate([{
             opacity: 0,
-            transform: 'scale(0.9)'
+            transform: 'scale(0.9)',
         }, {
             opacity: 1,
-            transform: 'scale(1)'
+            transform: 'scale(1)',
         }], {
             duration: 200,
-            easing: 'ease'
+            easing: 'ease',
         });
     }
 
     function closeSignInForm() {
         username = document.forms.signIn.login.value;
-        password = document.forms.signIn.password.value;
+        const password = document.forms.signIn.password.value;
         if (functionsService.sign(password)) {
             document.querySelector('#overlay').style.display = 'none';
             document.querySelector('#overlay').innerHTML = '';
@@ -281,97 +271,97 @@ var ui = (function () {
     }
 
     function openAddForm() {
-        var copy = document.querySelector('#add-new-form').content.cloneNode(true);
+        const copy = document.querySelector('#add-new-form').content.cloneNode(true);
         document.querySelector('#overlay').style.display = 'flex';
         document.querySelector('#overlay').appendChild(copy);
         document.querySelector('#overlay').firstElementChild.animate([{
             opacity: 0,
-            transform: 'scale(0.9)'
+            transform: 'scale(0.9)',
         }, {
             opacity: 1,
-            transform: 'scale(1)'
+            transform: 'scale(1)',
         }], {
             duration: 200,
-            easing: 'ease'
+            easing: 'ease',
         });
     }
 
     function closeAddForm() {
-        var article = {
-            id: '' + new Date().getTime(),
+        const article = {
+            id: `${new Date().getTime()}`,
             title: document.forms.add.title.value,
             summary: document.forms.add.summary.value,
             createdAt: new Date(),
             author: username,
             content: document.forms.add.content.value,
-            image: document.forms.add.image.value
+            image: document.forms.add.image.value,
         };
         if (articlesService.add(article)) {
             document.querySelector('#overlay').innerHTML = '';
             document.querySelector('#overlay').style.display = 'none';
             functionsService.show();
         } else {
-            alert('incorrect');}
+            alert('incorrect');
+        }
     }
 
     function openEditForm(id) {
-        var copy = document.querySelector('#edit-new-form').content.cloneNode(true);
+        const copy = document.querySelector('#edit-new-form').content.cloneNode(true);
         document.querySelector('#overlay').style.display = 'flex';
         document.querySelector('#overlay').appendChild(copy);
         document.querySelector('#overlay').firstElementChild.animate([{
             opacity: 0,
-            transform: 'scale(0.9)'
+            transform: 'scale(0.9)',
         }, {
             opacity: 1,
-            transform: 'scale(1)'
+            transform: 'scale(1)',
         }], {
             duration: 200,
-            easing: 'ease'
+            easing: 'ease',
         });
 
-        var article = articlesService.getArticle(id);
+        const article = articlesService.getArticle(id);
         document.forms.edit.id = id;
         document.forms.edit.title.value = article.title;
         document.forms.edit.summary.value = article.summary;
         document.forms.edit.content.value = article.content;
         document.forms.edit.image.value = article.image;
-
     }
 
     function closeEditForm() {
-        var article = {
+        const article = {
             id: document.forms.edit.id,
             title: document.forms.edit.title.value,
             summary: document.forms.edit.summary.value,
             createdAt: new Date(),
             author: username,
             content: document.forms.edit.content.value,
-            image: document.forms.edit.image.value
+            image: document.forms.edit.image.value,
         };
         if (articlesService.edit(article.id, article)) {
             functionsService.show();
             document.querySelector('#overlay').innerHTML = '';
             document.querySelector('#overlay').style.display = 'none';
         } else {
-            alert("incorrect");
+            alert('incorrect');
         }
     }
 
     function openReadMoreForm(id) {
-        var copy = document.querySelector('#read-more-form').content.cloneNode(true);
+        const copy = document.querySelector('#read-more-form').content.cloneNode(true);
         document.querySelector('#overlay').style.display = 'flex';
         document.querySelector('#overlay').appendChild(copy);
         document.querySelector('#overlay').firstElementChild.animate([{
             opacity: 0,
-            transform: 'scale(0.9)'
+            transform: 'scale(0.9)',
         }, {
             opacity: 1,
-            transform: 'scale(1)'
+            transform: 'scale(1)',
         }], {
             duration: 200,
-            easing: 'ease'
+            easing: 'ease',
         });
-        var article = serverWorker.getArticle(id);
+        const article = serverWorker.getArticle(id);
         document.forms.read.querySelector('img').src = article.image;
         document.forms.read.querySelector('h1').innerText = article.title;
         document.forms.read.querySelector('h2').innerText = article.summary;
@@ -383,20 +373,20 @@ var ui = (function () {
         document.querySelector('#overlay').style.display = 'none';
     }
 
-    document.querySelector('#overlay').addEventListener('click', function (event) {
+    document.querySelector('#overlay').addEventListener('click', (event) => {
         if (event.target === document.querySelector('#overlay')) {
             close();
         }
     });
 
     return {
-        openSignInForm: openSignInForm,
-        closeSignInForm: closeSignInForm,
-        openAddForm: openAddForm,
-        closeAddForm: closeAddForm,
-        openEditForm: openEditForm,
-        closeEditForm: closeEditForm,
-        openReadMoreForm: openReadMoreForm,
-        close: close
-    }
-})();
+        openSignInForm,
+        closeSignInForm,
+        openAddForm,
+        closeAddForm,
+        openEditForm,
+        closeEditForm,
+        openReadMoreForm,
+        close,
+    };
+}());
--- UI/public/serverWorker.js
+++ UI/public/serverWorker.js
@@ -1,32 +1,62 @@
-var serverWorker = (function () {
+const serverWorker = (function () {
     function getArticles() {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET","/getArticles", false);
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '/getArticles', false);
         xhr.send();
-        var articles = JSON.parse(xhr.responseText);
-        articles.forEach(function (art) {
+        const articles = JSON.parse(xhr.responseText);
+        articles.forEach((art) => {
             art.createdAt = new Date(art.createdAt);
         });
         return articles;
     }
 
     function getArticle(id) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET","/getArticle/"+id,false);
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `/getArticle/${id}`, false);
         xhr.send();
-        var article = JSON.parse(xhr.responseText);
+        const article = JSON.parse(xhr.responseText);
         article.createdAt = new Date(article.createdAt);
         return article;
     }
 
     function removeArticle(id) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "/delete/"+id,false);
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `/delete/${id}`, false);
         xhr.send();
     }
-    return {
-        getArticles:getArticles,
-        getArticle:getArticle,
-        removeArticle:removeArticle
+    function globalPost(articles) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '/news');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(articles));
     }
+    function updateArticle(article) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('PUT', '/newsEdit', false);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(article));
+    }
+    function getFullArticle(id) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '/news/' + id, false);
+        xhr.send();
+        const article = JSON.parse(xhr.responseText);
+        article.CreatedAt = new Date(article.CreatedAt);
+        return article;
+    }
+    function sendArticle(article) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('PUT', '/news');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(article));
+    }
+    return {
+        getArticles,
+        getArticle,
+        removeArticle,
+        globalPost,
+        updateArticle,
+        getFullArticle,
+        sendArticle,
+    };
 }());
