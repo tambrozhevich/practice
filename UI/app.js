e84da25673396408bb419937a5b8427007600560
ambrozhevich <tambrojevich@gmail.com>
2017-05-08 12:56:54 +0300

    9 task

--- UI/app.js
+++ UI/app.js
@@ -1,25 +1,24 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var port = 3000;
-var db = require('diskdb');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const db = require('diskdb');
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 db.connect('private', ['articles', 'users', 'deletedArticles']);
 
-app.get('/checkUser', function (req, res) {
-    var userCurrent = req.query.user;
-    var passwordCurrent = req.query.password;
-    console.log(userCurrent + " " + passwordCurrent);
+app.get('/checkUser', (req, res) => {
+    const userCurrent = req.query.user;
+    const passwordCurrent = req.query.password;
+    console.log(`${userCurrent} ${passwordCurrent}`);
     if (!userCurrent || !passwordCurrent) {
-        console.log("Not right request");
+        console.log('Not right request');
         res.status(400);
         res.json(false);
-    }
-    else {
+    } else {
         res.status(200);
-        result = db.users.forEach(function (f) {
+        db.users.forEach((f) => {
             if (f.name === userCurrent && f.password === passwordCurrent) {
                 res.json(true);
             }
@@ -27,51 +26,47 @@ app.get('/checkUser', function (req, res) {
     }
 });
 
-app.get('/getUsers', function (req, res) {
+app.get('/getUsers', (req, res) => {
     res.json(db.users.find());
 });
-
-app.get('/getActiveUser', function (req, res) {
+const activeUser = { username: null };
+app.get('/getActiveUser', (req, res) => {
     res.json(activeUser.username);
     res.status(200);
 });
 
-app.get('/getArticles', function (req, res) {
+app.get('/getArticles', (req, res) => {
     res.json(db.articles.find());
 });
 
-app.get('/getArticle/:id',function (req, res) {
-    res.json(db.articles.findOne({id: req.params.id}));
+app.get('/getArticle/:id', (req, res) => {
+    res.json(db.articles.findOne({ id: req.params.id }));
 });
 
-app.get('/delete/:id', function (req, res) {
-    var article = db.articles.findOne({id: req.body.id});
+app.get('/delete/:id', (req, res) => {
+    const article = db.articles.findOne({ id: req.body.id });
     db.deletedArticles.save(article);
-    db.articles.remove({id: req.body.id});
+    db.articles.remove({ id: req.body.id });
     res.json(article);
 });
 
-app.post('/post', function (req, res) {
-    var iNickname = req.query.nickname || req.body.nickname;
+app.post('/news', (req, res) => {
+    const iNickname = req.query.nickname || req.body.nickname;
     if (!iNickname) {
-        res.json = ({verdict: false, explanation: "Not right request"});
+        res.json = ({ verdict: false, explanation: 'Not right request' });
         res.status(400);
-    }
-    else {
+    } else {
         activeUser.username = iNickname;
         res.json(true);
         res.status(200);
     }
 });
 
-app.put('/putArticle', function (req, res) {
-    var article = req.body;
+app.put('/putArticle', (req, res) => {
+    const article = req.body;
     db.articles.push(article);
     res.json(true);
     res.status(200);
 });
-
-var activeUser = {username: null};
-
 app.listen(port);
-console.log("app is listening on port: " + port);
+console.log(`app is listening on port: ${port}`);
